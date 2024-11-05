d915303debf739a1508955457dd392c954dc50a5
nonick <87268971+nonick-mc@users.noreply.github.com>
2024-11-05 13:41:43 +0900

    feat: リンクを変数化

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import { Logo } from '@/components/logo';
+import { links } from '@/lib/constants/links';
 import { Icon } from '@iconify/react';
 import { Button } from '@nextui-org/button';
 import { Link } from '@nextui-org/link';
@@ -13,31 +14,19 @@ export function Navbar() {
       <NavbarContent justify='end'>
         <NavbarItem>
           <div className='flex items-center gap-1'>
-            <Button
-              as={Link}
-              href='https://docs.nonick-js.com'
-              variant='light'
-              isExternal
-              isIconOnly
-            >
+            <Button as={Link} href={links.docs} variant='light' isExternal isIconOnly>
               <Icon
                 icon='solar:book-bookmark-minimalistic-bold'
                 className='text-[24px] text-default-500'
               />
             </Button>
-            <Button
-              as={Link}
-              href='https://discord.gg/q4FcBm2P42'
-              variant='light'
-              isExternal
-              isIconOnly
-            >
+            <Button as={Link} href={links.discord} variant='light' isExternal isIconOnly>
               <Icon icon='akar-icons:discord-fill' className='text-[24px] text-default-500' />
             </Button>
           </div>
         </NavbarItem>
         <NavbarItem>
-          <Button as={Link} href='https://dashboard.nonick-js.com' color='primary' isExternal>
+          <Button as={Link} href={links.dashboard} color='primary' isExternal>
             ダッシュボード
           </Button>
         </NavbarItem>
---
+++ src/lib/constants/links.ts
@@ -0,0 +1,5 @@
+export const links = {
+  discord: 'https://discord.gg/q4FcBm2P42',
+  docs: 'https://docs.nonick-js.com',
+  dashboard: 'https://dashboard.nonick-js.com',
+};
