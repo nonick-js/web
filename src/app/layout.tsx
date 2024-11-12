bec6629065ecf430e0660c068c3181802d1d837d
nonick <87268971+nonick-mc@users.noreply.github.com>
2024-11-13 02:52:03 +0900

    hotfix: cssがデプロイ環境で適切に読み込まれない問題

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css';
+import '@/app/globals.css';
 import type { Metadata, Viewport } from 'next';
 import { Noto_Sans_JP } from 'next/font/google';
 import { Provider } from './provider';
